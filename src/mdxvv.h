fd024d09020df246b1909018a9a862d664dac5fe
tantanGH <tantangh68@gmail.com>
2023-01-21 10:22:19 +0900

    cache clear

--- src/mdxvv.h
+++
@@ -1,9 +0,0 @@
-#ifndef __H_MDXVV__
-#define __H_MDXVV__
-
-#define VERSION     "0.4.0 built with m68k-elf-gcc 12.2.0 (2023.01.21)"
-
-#define MXP_EXEC    "mxp.x"
-#define MMDSP_EXEC  "mmdsp.r"
-
-#endif
